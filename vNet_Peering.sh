5d62d316230ce7b4fccfd4fc4061f5783a7a8e3b
Hema Siva Kishore Vuyyuru <hemasivakishore@gmail.com>
2023-01-20 21:10:16 +0530

    Created vNet Peering between 1-2 2-1, 2-3 3-2, 3-1 1-3

--- vNet_Peering.sh
+++ vNet_Peering.sh
@@ -12,8 +12,16 @@ az vm create -g ${RG} -n LVM-1 --image UbuntuLTS --vnet-name ${RG}-vNet-1 --subn
 
 az vm create -g ${RG} -n LVM-2 --image UbuntuLTS --vnet-name ${RG}-vNet-2 --subnet ${RG}-Subnet-1 --admin-username adminhsk --admin-password "Bharath@123456" --size Standard_B1s --nsg ""
 
-az vm create -g ${RG} -n LVM-3 --image UbuntuLTS --vnet-name ${RG}-vNet-2 --subnet ${RG}-Subnet-1 --admin-username adminhsk --admin-password "Bharath@123456" --size Standard_B1s --nsg ""
+az vm create -g ${RG} -n LVM-3 --image UbuntuLTS --vnet-name ${RG}-vNet-3 --subnet ${RG}-Subnet-1 --admin-username adminhsk --admin-password "Bharath@123456" --size Standard_B1s --nsg ""
 
-az network vnet peering create --name vNet-1TOvNet-2 --vnet-name VN${RG}-vNet-1 --remote-vnet ${RG}-vNet-2 --resource-group ${RG} --allow-vnet-access --allow-forwarded-traffic
+az network vnet peering create --name vNet-1TOvNet-2 --vnet-name ${RG}-vNet-1 --remote-vnet ${RG}-vNet-2 --resource-group ${RG} --allow-vnet-access --allow-forwarded-traffic
 
-az network vnet peering create --name vNet-2TOvNet-1 --vnet-name VN${RG}-vNet-2 --remote-vnet ${RG}-vNet-1 --resource-group ${RG} --allow-vnet-access --allow-forwarded-traffic
+az network vnet peering create --name vNet-2TOvNet-1 --vnet-name ${RG}-vNet-2 --remote-vnet ${RG}-vNet-1 --resource-group ${RG} --allow-vnet-access --allow-forwarded-traffic
+
+az network vnet peering create --name vNet-2TOvNet-3 --vnet-name ${RG}-vNet-2 --remote-vnet ${RG}-vNet-3 --resource-group ${RG} --allow-vnet-access --allow-forwarded-traffic
+
+az network vnet peering create --name vNet-3TOvNet-2 --vnet-name ${RG}-vNet-3 --remote-vnet ${RG}-vNet-2 --resource-group ${RG} --allow-vnet-access --allow-forwarded-traffic
+
+az network vnet peering create --name vNet-3TOvNet-1 --vnet-name ${RG}-vNet-3 --remote-vnet ${RG}-vNet-1 --resource-group ${RG} --allow-vnet-access --allow-forwarded-traffic
+
+az network vnet peering create --name vNet-1TOvNet-3 --vnet-name ${RG}-vNet-1 --remote-vnet ${RG}-vNet-3 --resource-group ${RG} --allow-vnet-access --allow-forwarded-traffic
