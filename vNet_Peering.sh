c67daa77d37794826280a9a0a6708cfb210282a0
Hema Siva Kishore Vuyyuru <hemasivakishore@gmail.com>
2023-01-20 18:46:53 +0530

    VM-1 is created

--- vNet_Peering.sh
+++ vNet_Peering.sh
@@ -1,4 +1,4 @@
-export RG = Azure_vNet_Peering
+export RG=Azure_vNet_Peering
 
 az group create --location eastus -n ${RG}
 
@@ -7,3 +7,5 @@ az network vnet create -g ${RG} -n ${RG}-vNet-1 --address-prefix 10.26.0.0/16 --
 az network vnet create -g ${RG} -n ${RG}-vNet-2 --address-prefix 172.168.0.0/16 --subnet-name ${RG}-Subnet-1 --subnet-prefix 172.168.1.0/24 -l eastus
 
 az network vnet create -g ${RG} -n ${RG}-vNet-3 --address-prefix 192.168.0.0/16 --subnet-name ${RG}-Subnet-1 --subnet-prefix 192.168.1.0/24 -l westus
+
+az vm create -g ${RG} -n LVM-1 --image UbuntuLTS --vnet-name ${RG}-vNet-1 --subnet ${RG}-Subnet-1 --admin-username adminhsk --admin-password "Bharath@123456" --size Standard_B1s --nsg ""
